fdd15900ac27cb4e55cee4ab29741afd2258bc21
renatoia <renatoadsfatecfranca@gmail.com>
2018-04-27 19:42:15 -0300

    20180427_2

--- src/services/auth.config.ts
+++ src/services/auth.config.ts
@@ -6,5 +6,5 @@ export const AUTH_CONFIG = {
   // Needed for Auth0Cordova (capitalization: Id):
   clientId: 'RwAf9lj2c6yFwEc1ZlpoATEx1BdzCdSt',
   domain: 'renatoalmeida.auth0.com', // e.g., you.auth0.com
-  packageIdentifier: 'br.gov.sp.fatec.franca.renatoalmeida.loja' // config.xml widget ID
+  packageIdentifier: 'com.auth0.ionic' // config.xml widget ID
 };
