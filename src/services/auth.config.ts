c1cde7af64ad4a203f7e485ac7cd43e255cf8d36
renatoia <renatoadsfatecfranca@gmail.com>
2018-04-26 22:22:13 -0300

    Teste

--- src/services/auth.config.ts
+++ src/services/auth.config.ts
@@ -2,9 +2,9 @@
 
 export const AUTH_CONFIG = {
   // Needed for Auth0 (capitalization: ID):
-  clientID: '{Auth0_Client_ID}',
+  clientID: 'RwAf9lj2c6yFwEc1ZlpoATEx1BdzCdSt',
   // Needed for Auth0Cordova (capitalization: Id):
-  clientId: '{Auth0_Client_ID}',
-  domain: '{Auth0_Domain}', // e.g., you.auth0.com
+  clientId: 'RwAf9lj2c6yFwEc1ZlpoATEx1BdzCdSt',
+  domain: 'renatoalmeida.auth0.com', // e.g., you.auth0.com
   packageIdentifier: 'com.auth0.ionic' // config.xml widget ID
 };
