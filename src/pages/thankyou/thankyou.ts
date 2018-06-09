f6ffc4e6e7511992e0f3ff292f4ae666f392fd75
renatoia <renatoadsfatecfranca@gmail.com>
2018-06-08 23:37:03 -0300

    Parte 1 - Atualização

--- src/pages/thankyou/thankyou.ts
+++ src/pages/thankyou/thankyou.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'thank-you',
   templateUrl: 'thankyou.html',
